1fcc3fabf3e77b7d7962171aec7949385be9bf91
Augusto Silva <augusto.da.silva@live.com>
2022-05-09 11:36:48 -0300

    for /each

--- desafios/03_laços_quantificados /3.3.rb
+++ desafios/03_laços_quantificados /3.3.rb
@@ -18,3 +18,10 @@ for x in 1..num
         puts x
     end
 end
+
+# Solução Each
+1..num.each do |i|
+    if x % 2 == 0
+        puts x
+    end
+end
--- desafios/03_laços_quantificados /3.5.rb
+++ desafios/03_laços_quantificados /3.5.rb
@@ -2,3 +2,27 @@
 # diga quantos são pares e quantos são ímpares. Imprima também a soma dos números pares, e a soma 
 # dos números ímpares.
 
+print "Quantos números deseja ler: "
+n = gets.chomp.to_i
+
+num_par = 0 
+num_impar = 0 
+soma_par = 0
+soma_impar = 0
+
+n.times do
+    print "Digite um número inteiro: "
+    num = gets.chomp.to_i
+    if num % 2 == 0 then
+        num_par += 1
+        soma_par += num
+    else
+        num_impar += 1
+        soma_impar += num
+    end 
+end
+
+puts "Dos #{n} números informados, #{num_par} são pares e #{num_impar} são impares."
+puts "A soma dos número pares: #{soma_par}"
+puts "A soma dos números impares: #{soma_impar}"
+
