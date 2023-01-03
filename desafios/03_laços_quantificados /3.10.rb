6c20e53b86e0a8624835ea1c91817ca85ef85ee5
Gus-96 <augusto.da.silva@live.com>
2023-01-03 00:18:28 -0300

    laço de repetição

--- desafios/03_laços_quantificados /3.10.rb
+++ desafios/03_laços_quantificados /3.10.rb
@@ -1,8 +1,15 @@
 # Crie um script em Ruby que imprima todas as tabuadas de multiplicação de 1 a 10.
-
+u = 1
 num = 1
-i = 0
-while i < 11
-  puts "#{num} X #{i} = #{num*i}"
-  i += 1
+while u < 11
+
+  i = 0
+  while i < 11
+    puts "#{num} X #{i} = #{num*i}"
+    i += 1
+  end
+  puts "-------------------------------"
+
+  u += 1
+  num += 1
 end
